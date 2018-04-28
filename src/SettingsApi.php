1972e0ff7f7cb95fd5b1de661c7106ff9f3db223
JaxsonWang <wangjin@boxrom.com>
2018-04-28 16:23:22 +0800

    兼容php 7.2 感谢@weijer

--- src/SettingsApi.php
+++ src/SettingsApi.php
@@ -103,7 +103,10 @@ class SettingsApi {
 
 			if ( isset( $section['desc'] ) && ! empty( $section['desc'] ) ) {
 				$section['desc'] = '<div class="inside">' . $section['desc'] . '</div>';
-				$callback        = create_function( '', 'echo "' . str_replace( '"', '\"', $section['desc'] ) . '";' );
+				//$callback        = create_function( '', 'echo "' . str_replace( '"', '\"', $section['desc'] ) . '";' );
+			    $callback        = function () use ($section) {
+			        echo str_replace('"', '\"', $section['desc']);
+                };
 			} else if ( isset( $section['callback'] ) ) {
 				$callback = $section['callback'];
 			} else {
